ae86f95bda1eb4eda9ee852bf1342a47aaa8c406
Guzhov Roman <guzovvrom277@gmail.com>
2025-12-14 14:38:31 +0500

    Добавлены ссылки на главную страницу

--- imperial_database/characters/urls.py
+++ imperial_database/characters/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='main_page'),
     path('<str:person_name>/', views.show_person, name='show_person'),
     path('starship/<str:starship_name>/', views.show_starship, name='show_starship')
 ]
