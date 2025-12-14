fdd100a39b19348167d92cacb53dcfed7b8f1773
Guzhov Roman <guzovvrom277@gmail.com>
2025-12-14 15:27:25 +0500

    добавлена админ панель

--- imperial_database/characters/admin.py
+++ imperial_database/characters/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Characters, Starships
 
-# Register your models here.
+# Регистрируем модели, чтобы они появились в админке
+admin.site.register(Characters)
+admin.site.register(Starships)
